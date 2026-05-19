90ae744c19d74925b9ce81f00298a7143b348bdd
skotarov <skotarov@gmail.com>
2026-05-19 10:54:15 +0300

    Add Android web view activity

---
+++ mobile/calllog-android/app/src/main/java/com/onlineimoti/calllog/WebViewActivity.kt
@@ -0,0 +1,52 @@
+package com.onlineimoti.calllog
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.view.View
+import android.webkit.WebChromeClient
+import android.webkit.WebResourceRequest
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.appcompat.app.AppCompatActivity
+import com.onlineimoti.calllog.databinding.ActivityWebViewBinding
+
+class WebViewActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityWebViewBinding
+
+    @SuppressLint("SetJavaScriptEnabled")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityWebViewBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val url = intent.getStringExtra(EXTRA_URL).orEmpty()
+        if (url.isBlank()) {
+            finish()
+            return
+        }
+
+        binding.webView.settings.javaScriptEnabled = true
+        binding.webView.settings.domStorageEnabled = true
+        binding.webView.settings.loadsImagesAutomatically = true
+        binding.webView.webChromeClient = WebChromeClient()
+        binding.webView.webViewClient = object : WebViewClient() {
+            override fun onPageStarted(view: WebView?, url: String?, favicon: android.graphics.Bitmap?) {
+                binding.webLoadingText.visibility = View.VISIBLE
+            }
+
+            override fun onPageFinished(view: WebView?, url: String?) {
+                binding.webLoadingText.visibility = View.GONE
+            }
+
+            override fun shouldOverrideUrlLoading(view: WebView?, request: WebResourceRequest?): Boolean {
+                return false
+            }
+        }
+
+        binding.webView.loadUrl(url)
+    }
+
+    companion object {
+        const val EXTRA_URL = "url"
+    }
+}
