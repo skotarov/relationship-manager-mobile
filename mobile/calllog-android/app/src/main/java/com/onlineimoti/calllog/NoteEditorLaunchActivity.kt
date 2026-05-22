0bdd210aa69a571cfae8f1b6e16f688973255379
skotarov <skotarov@gmail.com>
2026-05-22 15:25:50 +0300

    Add note editor notification launcher activity

---
+++ mobile/calllog-android/app/src/main/java/com/onlineimoti/calllog/NoteEditorLaunchActivity.kt
@@ -0,0 +1,45 @@
+package com.onlineimoti.calllog
+
+import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+import android.provider.Settings
+import androidx.core.app.NotificationManagerCompat
+
+class NoteEditorLaunchActivity : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        openEditorAndFinish()
+    }
+
+    override fun onNewIntent(intent: Intent?) {
+        super.onNewIntent(intent)
+        setIntent(intent)
+        openEditorAndFinish()
+    }
+
+    private fun openEditorAndFinish() {
+        NotificationManagerCompat.from(this).apply {
+            cancel(LOOKUP_NOTIFICATION_ID)
+            cancel(LOOKUP_SHADE_NOTIFICATION_ID)
+            cancel(CallReportRuntime.POST_CALL_NOTIFICATION_ID)
+        }
+
+        if (Settings.canDrawOverlays(this)) {
+            startService(
+                Intent(this, PostCallOverlayService::class.java)
+                    .putExtra(PostCallOverlayService.EXTRA_MODE, PostCallOverlayService.MODE_NOTE)
+                    .putExtra(PostCallOverlayService.EXTRA_PHONE, intent?.getStringExtra(PostCallOverlayService.EXTRA_PHONE).orEmpty())
+                    .putExtra(PostCallOverlayService.EXTRA_DIRECTION, intent?.getStringExtra(PostCallOverlayService.EXTRA_DIRECTION).orEmpty())
+                    .putExtra(PostCallOverlayService.EXTRA_TITLE, intent?.getStringExtra(PostCallOverlayService.EXTRA_TITLE).orEmpty())
+            )
+        }
+        finishAndRemoveTask()
+        overridePendingTransition(0, 0)
+    }
+
+    companion object {
+        private const val LOOKUP_NOTIFICATION_ID = 2001
+        private const val LOOKUP_SHADE_NOTIFICATION_ID = 2004
+    }
+}
