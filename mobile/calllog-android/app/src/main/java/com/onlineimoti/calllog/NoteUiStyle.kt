2505bee4009c9f28095984e2261007f88ee3b067
skotarov <skotarov@gmail.com>
2026-05-27 13:21:09 +0300

    Add shared note UI style matrix

---
+++ mobile/calllog-android/app/src/main/java/com/onlineimoti/calllog/NoteUiStyle.kt
@@ -0,0 +1,29 @@
+package com.onlineimoti.calllog
+
+import android.graphics.Color
+
+internal data class NoteCardColors(
+    val text: Int,
+    val mutedText: Int,
+    val metaText: Int,
+    val background: Int,
+    val border: Int,
+)
+
+internal object NoteUiStyle {
+    val General = NoteCardColors(
+        text = Color.rgb(92, 64, 0),
+        mutedText = Color.rgb(100, 116, 139),
+        metaText = Color.rgb(92, 64, 0),
+        background = Color.WHITE,
+        border = Color.rgb(226, 232, 240),
+    )
+
+    val Call = NoteCardColors(
+        text = Color.rgb(8, 47, 73),
+        mutedText = Color.rgb(7, 89, 133),
+        metaText = Color.rgb(7, 89, 133),
+        background = Color.rgb(224, 246, 255),
+        border = Color.rgb(125, 211, 252),
+    )
+}
