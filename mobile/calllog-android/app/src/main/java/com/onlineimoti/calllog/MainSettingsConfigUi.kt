bfbf4403b54f644ddfcc743b436ed1d81c2382d7
skotarov <skotarov@gmail.com>
2026-05-29 13:23:05 +0300

    Wire overlay popup setting into settings UI

--- mobile/calllog-android/app/src/main/java/com/onlineimoti/calllog/MainSettingsConfigUi.kt
+++ mobile/calllog-android/app/src/main/java/com/onlineimoti/calllog/MainSettingsConfigUi.kt
@@ -16,6 +16,8 @@ object MainSettingsConfigUi {
         binding.formPathInput.setText(config.formPath)
         binding.historyPathInput.setText(config.historyPath)
         binding.postCallTimeoutInput.setText(config.postCallPromptTimeoutSeconds.toString())
+        binding.useOverlayPopupsCheckBox.isChecked = config.useOverlayPopups
+        binding.overlayPopupOptionsGroup.visibility = if (config.useOverlayPopups) View.VISIBLE else View.GONE
         binding.useCustomStartPopupCheckBox.isChecked = config.useCustomStartPopup
         binding.useCustomEndPopupCheckBox.isChecked = config.useCustomEndPopup
         if (config.contactLinkMode == ConfigStore.CONTACT_LINK_MODE_CONTACT) {
@@ -38,6 +40,7 @@ object MainSettingsConfigUi {
             historyPath = binding.historyPathInput.text?.toString().orEmpty(),
             postCallPromptTimeoutSeconds = binding.postCallTimeoutInput.text?.toString()?.toIntOrNull()
                 ?: ConfigStore.DEFAULT_POST_CALL_TIMEOUT_SECONDS,
+            useOverlayPopups = binding.useOverlayPopupsCheckBox.isChecked,
             useCustomStartPopup = binding.useCustomStartPopupCheckBox.isChecked,
             useCustomEndPopup = binding.useCustomEndPopupCheckBox.isChecked,
             contactLinkMode = if (binding.contactLinkModeContact.isChecked) {
