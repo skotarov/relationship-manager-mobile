45f9adde02faef77ae9a93d8ffdecd005681e843
skotarov <skotarov@gmail.com>
2026-06-01 12:52:46 +0300

    Add build version settings helper

---
+++ mobile/calllog-android/app/src/main/java/com/onlineimoti/calllog/MainBuildVersion.kt
@@ -0,0 +1,20 @@
+package com.onlineimoti.calllog
+
+import androidx.appcompat.app.AppCompatActivity
+import com.onlineimoti.calllog.databinding.ActivityMainBinding
+
+internal object MainBuildVersion {
+    fun render(activity: AppCompatActivity, binding: ActivityMainBinding) {
+        val packageInfo = runCatching {
+            activity.packageManager.getPackageInfo(activity.packageName, 0)
+        }.getOrNull()
+        val versionName = packageInfo?.versionName ?: BuildConfig.VERSION_NAME
+        val versionCode = if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.P) {
+            packageInfo?.longVersionCode?.toString()
+        } else {
+            @Suppress("DEPRECATION")
+            packageInfo?.versionCode?.toString()
+        } ?: BuildConfig.VERSION_CODE.toString()
+        binding.buildVersionText.text = "Build version: $versionName ($versionCode)"
+    }
+}
