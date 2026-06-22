847f446be173e2db71ebb136366348b3c2042238
skotarov <skotarov@gmail.com>
2026-06-22 14:54:58 +0300

    Remove duplicate permission summary bridge

--- mobile/calllog-android/app/src/main/java/com/onlineimoti/calllog/StatusCompatibility.kt
+++
@@ -1,9 +0,0 @@
-package com.onlineimoti.calllog
-
-import com.onlineimoti.calllog.databinding.ActivityMainBinding
-
-internal object MainPermissionSummary {
-    fun refresh(activity: MainActivity, binding: ActivityMainBinding) {
-        PermissionStatusRenderer.refresh(activity, binding)
-    }
-}
