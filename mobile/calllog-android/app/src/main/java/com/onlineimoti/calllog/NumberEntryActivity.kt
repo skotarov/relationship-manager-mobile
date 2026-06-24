273262374837adec24642bb6c0e8141c748872fd
skotarov <skotarov@gmail.com>
2026-06-25 00:15:08 +0300

    Fix number keypad ImageView import

--- mobile/calllog-android/app/src/main/java/com/onlineimoti/calllog/NumberEntryActivity.kt
+++ mobile/calllog-android/app/src/main/java/com/onlineimoti/calllog/NumberEntryActivity.kt
@@ -9,6 +9,7 @@ import android.os.Bundle
 import android.view.Gravity
 import android.view.View
 import android.widget.ImageButton
+import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.TextView
 import android.widget.Toast
