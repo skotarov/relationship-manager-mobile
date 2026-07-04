1ed72d428d78fde5e404ba9ba87c6d5c4a1e8b6b
skotarov <skotarov@gmail.com>
2026-07-04 11:41:23 +0300

    Align Kotlin compiler with Play Billing Kotlin metadata

--- mobile/calllog-android/build.gradle.kts
+++ mobile/calllog-android/build.gradle.kts
@@ -1,4 +1,6 @@
 plugins {
     id("com.android.application") version "8.7.3" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.25" apply false
+    // Billing KTX and one of its Kotlin transitive artifacts use Kotlin 2.2 metadata.
+    // Keep the compiler aligned instead of forcing an incompatible Kotlin stdlib downgrade.
+    id("org.jetbrains.kotlin.android") version "2.2.0" apply false
 }
