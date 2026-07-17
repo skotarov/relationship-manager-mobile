0f17525170f15ef0a280856009987e33a5b0d2b4
skotarov <skotarov@gmail.com>
2026-07-17 15:40:32 +0300

    Auto-save list theme selection

--- mobile/calllog-android/app/src/main/java/com/onlineimoti/calllog/MainSettingsAutoSaveController.kt
+++ mobile/calllog-android/app/src/main/java/com/onlineimoti/calllog/MainSettingsAutoSaveController.kt
@@ -50,6 +50,9 @@ internal class MainSettingsAutoSaveController(
         popupFilter.notifyUnknownContactsCheckBox.autoSaveCheckedChanges()
         popupFilter.notifyKnownContactsCheckBox.autoSaveCheckedChanges()
         tests.showRmDebugBoxCheckBox.autoSaveCheckedChanges()
+        binding.settingsGeneralGroup.listThemeGroup.setOnCheckedChangeListener { _, _ ->
+            autoSaveSettings()
+        }
         binding.settingsGeneralGroup.fontScaleGroup.setOnCheckedChangeListener { _, checkedId ->
             autoSaveSettings()
             val scale = when (checkedId) {
