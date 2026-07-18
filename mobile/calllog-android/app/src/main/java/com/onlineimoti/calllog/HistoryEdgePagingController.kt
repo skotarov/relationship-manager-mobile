b3663950c127bcf6496db011df3620ce3552ca07
skotarov <skotarov@gmail.com>
2026-07-18 14:50:23 +0300

    Apply selected paging mode to History

--- mobile/calllog-android/app/src/main/java/com/onlineimoti/calllog/HistoryEdgePagingController.kt
+++ mobile/calllog-android/app/src/main/java/com/onlineimoti/calllog/HistoryEdgePagingController.kt
@@ -1,15 +1,20 @@
 package com.onlineimoti.calllog
 
+import android.content.Context
 import android.widget.LinearLayout
 import android.widget.ScrollView
 
 /** Connects cumulative History rendering to shared buffered paging. */
 internal class HistoryEdgePagingController(
+    context: Context,
     private val history: CallReportMergedHistoryController,
 ) {
+    private val appContext = context.applicationContext
     private val delegate = EdgePageScrollController(
         canPrevious = history::canPreviousPage,
-        canNext = history::canNextPage,
+        canNext = {
+            PageLoadingModeStore.usesPrefetch(appContext) && history.canNextPage()
+        },
         previousPage = { history.previousPage() },
         nextPage = { history.nextPage() },
         pageReady = { true },
