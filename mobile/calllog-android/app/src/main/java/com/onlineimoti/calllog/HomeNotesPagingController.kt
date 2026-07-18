0573acfd233af3115cdb6569f14b38ab898b1793
skotarov <skotarov@gmail.com>
2026-07-18 16:50:09 +0300

    Resume Home prefetch after notes attach

---
+++ mobile/calllog-android/app/src/main/java/com/onlineimoti/calllog/HomeNotesPagingController.kt
@@ -0,0 +1,40 @@
+package com.onlineimoti.calllog
+
+import android.view.View
+import com.onlineimoti.calllog.databinding.ActivityHomeBinding
+
+internal class HomeEdgePagingController(
+    private val binding: ActivityHomeBinding,
+    canPrevious: () -> Boolean,
+    canNext: () -> Boolean,
+    previousPage: () -> Unit,
+    nextPage: () -> Unit,
+) {
+    private val delegate = EdgePageScrollController(
+        canPrevious = canPrevious,
+        canNext = { PageLoadingModeStore.usesPrefetch(binding.root.context) && canNext() },
+        previousPage = previousPage,
+        nextPage = nextPage,
+        pageReady = {
+            HomePageReadyState.isReady() &&
+                binding.paginationContainer.visibility == View.VISIBLE &&
+                binding.fullLogProgress.visibility != View.VISIBLE
+        },
+        retainPreviousPages = true,
+        protectRetainedPrefix = true,
+        pageToken = { binding.pageText.text.toString() },
+        prefetchNext = true,
+    )
+
+    init {
+        HomePageReadyState.setOnReady { bind() }
+    }
+
+    fun bind() = delegate.bind(binding.homeCallsScrollView, binding.homeCallsContainer)
+    fun cancel() = delegate.cancelPending()
+    fun isTransitioning(): Boolean = delegate.isTransitioning()
+    fun release() {
+        HomePageReadyState.clearOnReady()
+        delegate.release()
+    }
+}
