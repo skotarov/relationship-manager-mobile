e33d03f9f6249b70f2af79f6e95eadfffb44c5c8
skotarov <skotarov@gmail.com>
2026-07-18 14:01:16 +0300

    Preserve compact list call sites without theme checks

--- mobile/calllog-android/app/src/main/java/com/onlineimoti/calllog/CompactListUi.kt
+++ mobile/calllog-android/app/src/main/java/com/onlineimoti/calllog/CompactListUi.kt
@@ -1,5 +1,6 @@
 package com.onlineimoti.calllog
 
+import android.content.Context
 import android.graphics.drawable.GradientDrawable
 import android.view.View
 import android.view.ViewGroup
@@ -14,7 +15,11 @@ internal object ListThemeUi {
     private val pendingParents = Collections.newSetFromMap(WeakHashMap<ViewGroup, Boolean>())
     private val originalRadii = WeakHashMap<View, Float>()
 
-    fun applyRowSpacing(view: View, dp: (Int) -> Int): View {
+    fun applyRowSpacing(
+        view: View,
+        @Suppress("UNUSED_PARAMETER") context: Context,
+        dp: (Int) -> Int,
+    ): View {
         val params = view.layoutParams as? ViewGroup.MarginLayoutParams ?: return view
         rememberRadius(view, dp)
         groupedRows.add(view)
