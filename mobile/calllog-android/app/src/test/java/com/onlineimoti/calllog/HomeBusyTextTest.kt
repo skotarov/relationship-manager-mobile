c96d6ded577057b087432e62e94b376e9b4cc0f7
skotarov <skotarov@gmail.com>
2026-07-21 23:47:18 +0300

    Test History busy tooltip messages

--- mobile/calllog-android/app/src/test/java/com/onlineimoti/calllog/HomeBusyTextTest.kt
+++ mobile/calllog-android/app/src/test/java/com/onlineimoti/calllog/HomeBusyTextTest.kt
@@ -10,6 +10,9 @@ class HomeBusyTextTest {
         assertEquals("Търся в разговори и бележки…", HomeBusyText.value(HomeBusyWork.SEARCH, bulgarian = true))
         assertEquals("Зареждам пълната история…", HomeBusyText.value(HomeBusyWork.FULL_LOG, bulgarian = true))
         assertEquals("Зареждам още разговори…", HomeBusyText.value(HomeBusyWork.MORE_CALLS, bulgarian = true))
+        assertEquals("Зареждам локалната история…", HomeBusyText.value(HomeBusyWork.HISTORY_LOCAL, bulgarian = true))
+        assertEquals("Зареждам историята от сървъра…", HomeBusyText.value(HomeBusyWork.HISTORY_SERVER, bulgarian = true))
+        assertEquals("Подготвям историята…", HomeBusyText.value(HomeBusyWork.HISTORY_PREPARE, bulgarian = true))
     }
 
     @Test
@@ -17,5 +20,8 @@ class HomeBusyTextTest {
         assertEquals("Loading clients…", HomeBusyText.value(HomeBusyWork.CLIENTS, bulgarian = false))
         assertEquals("Adding server notes…", HomeBusyText.value(HomeBusyWork.SERVER_NOTES, bulgarian = false))
         assertEquals("Updating company data…", HomeBusyText.value(HomeBusyWork.COMPANY_DATA, bulgarian = false))
+        assertEquals("Loading local history…", HomeBusyText.value(HomeBusyWork.HISTORY_LOCAL, bulgarian = false))
+        assertEquals("Loading server history…", HomeBusyText.value(HomeBusyWork.HISTORY_SERVER, bulgarian = false))
+        assertEquals("Preparing history…", HomeBusyText.value(HomeBusyWork.HISTORY_PREPARE, bulgarian = false))
     }
 }
